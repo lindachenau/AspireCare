e3d275932d72b79fd1a571705254c5fc6158e882
Linda Chen <lindachenau@gmail.com>
2021-03-29 17:46:06 +1100

    Restructured GraphQL for easy maintenance

---
+++ src/utils/useAppointmentProfiles.js
@@ -0,0 +1,44 @@
+import { useStaticQuery, graphql } from "gatsby"
+export const useAppointmentProfiles = () => {
+  // Prepare doctor info from their markdown files and use gatsby-image to optimise their avatar profile
+  const { allMarkdownRemark, allFile } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(
+          sort: {
+            fields: [frontmatter___sequence], 
+            order: ASC
+          }, 
+          filter: {
+            frontmatter: {role: {regex: "/(appointment)/"}}, 
+            fileAbsolutePath: {regex: "/(team)/"}
+          }) {
+          edges {
+            node {
+              frontmatter {
+                bpid
+                title
+                job
+                image {
+                  id
+                }
+              }
+            }
+          }
+        }
+        allFile(
+          filter: {extension: {regex: "/(jpeg|jpg|png)/"}, sourceInstanceName: {eq: "team"}}
+        ) {
+          nodes {
+            id
+            childImageSharp {
+              fluid (maxWidth: 120) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }`)
+
+  return { allMarkdownRemark, allFile }
+}
